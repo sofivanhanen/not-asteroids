2fbd7a9109ae7e4c37feeb325439a1164cee7ba5
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-03-03 20:33:44 +0200

    Finalize 'withinSafetyZone'

--- Assets/Scripts/EarthTurretBehaviour.cs
+++ Assets/Scripts/EarthTurretBehaviour.cs
@@ -24,7 +24,7 @@ public class EarthTurretBehaviour : MonoBehaviour {
 	}
 
 	// Calculates whether an object will enter the safety zone or not
-	private bool TrajectoryWithinSafetyZone(Vector3 asteroidPosition, Vector3 asteroiVelocity) {
+	private bool TrajectoryWithinSafetyZone(Vector3 asteroidPosition, Vector3 asteroidVelocity) {
 		// We approach this problem by calculating the perpendicular distance
 		// between the path of the asteroid and the center of the danger zone.
 		// If the distance is smaller than the radius, we know the asteroid will
@@ -33,20 +33,25 @@ public class EarthTurretBehaviour : MonoBehaviour {
 		// this calculation does not take into account the size of the asteroid)
 		// For the perpendicular distance, we use the equation for a line defined
 		// by two points for its simplicity.
-		// Slope
-		float slope = asteroiVelocity.z / asteroiVelocity.x;
-		// TODO: Special case: slope = 0
-		// TODO: Check if actually going towards
+
 		// Define points
 		float x1 = asteroidPosition.x;
 		float z1 = asteroidPosition.z;
-		float x2 = x1 + 5; // arbitrary difference / delta
-		float z2 = z1 + (slope * 5); // slope = deltaZ / deltaX -> deltaZ = slope * deltaX
+		// Next position
+		float x2 = x1 + asteroidVelocity.x;
+		float z2 = z1 + asteroidVelocity.z;
 
 		// Equation derived on Wikipedia: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
 		// Since x0 = 0 and z0 = 0 (center of danger zone) equation is simplified:
 		float distance = Mathf.Abs(x2*z1 - z2*x1) / Mathf.Sqrt(Mathf.Pow(z2-z1, 2) + Mathf.Pow(x2-x1, 2));
 
-		return distance <= dangerZoneRadius;
+		if (distance > dangerZoneRadius)
+			return false;
+
+		// We still have to check if the asteroid is actually coming towards Earth.
+		// Using Pythagorean theorem. x0 and z0 = 0, so equation is simplified.
+		float firstDistance = Mathf.Sqrt(Mathf.Pow(x1, 2) + Mathf.Pow(z1, 2));
+		float secondDistance = Mathf.Sqrt(Mathf.Pow(x2, 2) + Mathf.Pow(z2, 2));
+		return secondDistance < firstDistance;
 	}
 }
