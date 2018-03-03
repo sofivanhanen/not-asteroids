22b65f509a914cfef7c18cd70d697a946bf686b1
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-03-03 13:22:14 +0200

    Create GameMaster, spawn asteroids on click

---
+++ Assets/ClickListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public GameObject asteroid;
+	
+	// Update is called once per frame
+	void Update () {
+		// If user clicked, spawn a new Asteroid.
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 position = new Vector3(-5, 0, 0);
+			Instantiate(asteroid, position, Quaternion.identity);
+		}
+	}
+}
